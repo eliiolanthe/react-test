af3bf991d3174d7e352e44190db59e5ab1d499eb
Elvira Paulin <eli.paulin@gmail.com>
2021-10-21 11:05:00 +0200

    paragraph added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from <code style={{ color: 'yellow' }}>develop</code> branch!
+          Hello from <code style={{ color: "yellow" }}>develop</code> branch!
+        </p>
+        <p>
+          Here's another paragraph.
         </p>
         <a
           className="App-link"
